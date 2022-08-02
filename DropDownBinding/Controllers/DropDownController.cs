ee69d3fbac6649a9e6422e3aae97a9ab0fa170e1
DELL <DELL@DESKTOP-KREPKH7>
2022-08-02 23:00:08 +0530

    Add project files.

---
+++ DropDownBinding/Controllers/DropDownController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownBinding.Controllers
+{
+    public class DropDownController : Controller
+    {
+        DAL.DataAccessLayer DAL = new DAL.DataAccessLayer();
+        // GET: DropDown
+        public ActionResult Index()
+        {
+            Country_Bind();
+            return View();
+        }
+
+        public void Country_Bind()
+        {
+            DataSet ds = DAL.Get_Country();
+            List<SelectListItem> countryList = new List<SelectListItem>();
+            foreach (DataRow dataRow in ds.Tables[0].Rows)
+            {
+                countryList.Add(new SelectListItem { Text = dataRow["CountryName"].ToString(), Value = dataRow["CountryID"].ToString() });
+            }
+            ViewBag.Country = countryList;
+        }
+        public JsonResult State_Bind(String country_id)
+        {
+            DataSet ds = DAL.Get_State(country_id);
+            List<SelectListItem> stateList = new List<SelectListItem>();
+            foreach (DataRow dataRow in ds.Tables[0].Rows)
+            {
+                stateList.Add(new SelectListItem { Text = dataRow["StateName"].ToString(), Value = dataRow["StateID"].ToString() });
+            }
+            return Json(stateList , JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult City_Bind(String state_id)
+        {
+            DataSet ds = DAL.Get_City(state_id);
+            List<SelectListItem> cityList = new List<SelectListItem>();
+            foreach (DataRow dataRow in ds.Tables[0].Rows)
+            {
+                cityList.Add(new SelectListItem { Text = dataRow["CityName"].ToString(), Value = dataRow["CityID"].ToString() });
+            }
+            return Json(cityList, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ DropDownBinding/DAL/DataAccessLayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DropDownBinding.DAL
+{
+    public class DataAccessLayer
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        public DataSet Get_Country()
+        {
+            SqlCommand cmd = new SqlCommand("Select * From tblCountry",con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            return ds;
+        }
+        public DataSet Get_State(string country_id)
+        {
+            SqlCommand cmd = new SqlCommand("Select * From tblState where CountryID = @catid", con);
+            cmd.Parameters.AddWithValue("@catid",country_id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            return ds;
+        }
+
+        public DataSet Get_City(string state_id)
+        {
+            SqlCommand cmd = new SqlCommand("Select * From tblCity where StateID = @stateid", con);
+            cmd.Parameters.AddWithValue("@stateid", state_id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            return ds;
+        }
+    }
+}
